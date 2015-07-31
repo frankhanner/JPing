829850fb2f2e3adec55142741f88b63cb68ea9b2
Frank <luapdeux@gmail.com>
2015-07-30 19:53:49 -0700

    Made TODO

--- Ping.java
+++ Ping.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
+// TODO(frankhanner): Why the hell isn't there unix support?! Add multi OS support.
 public class Ping {
-
     public static boolean isReachable(String host) throws IOException, InterruptedException {
         String cmd = "cmd /c ping -n 1 " + host + " | find \"TTL\"";
         Process proc = Runtime.getRuntime().exec(cmd);
